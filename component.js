2940c8aa525dfb2ee182fc8f3bb50e350b173c6b
Teixeira <claudioteixeira@outlook.com>
2022-02-25 11:40:16 -0300

    File component

---
+++ component.js
@@ -0,0 +1,23 @@
+sap.ui.define([
+	"sap/ui/core/UIComponent"
+], function(
+	UIComponent
+) {
+	"use strict";
+
+	return UIComponent.extend("buscacep.component", {
+		
+		metadata: {
+      manifest: "json"
+    },
+
+    init: function() {
+      UIComponent.prototype.init.apply(this, arguments)
+
+			// create tehe views based on the uri/hash
+			// inicializa as rotas (manifest)
+			this.getRouter().initialize();
+    }
+
+	});
+});
